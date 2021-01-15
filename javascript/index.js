7ea77805091fc05c800df2ab7c0c02d1207337bc
josethz00 <jtsoares17@hotmail.com>
2021-01-15 16:07:56 -0300

    feat: modifed some styles and added balls to the array

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,9 @@
 window.onload = () => {
 
     const betGrid = document.querySelector('[data-js=bet-grid]');
-    const betNumbers = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+    const betNumbers = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
     const betNumbersElements = [];
+    let markedNumbers = [];
 
     function createCssStyleTag () {
 
@@ -16,12 +17,22 @@ window.onload = () => {
 
     function populateBetGrid () {
 
-        for (let i = 0; i <= betNumbers.length; i++) {
+        for (let index = 0; index < betNumbers.length; index++) {
 
-            styleTag.innerHTML = `.${betNumbers[i]} { grid-area: ${betNumbers[i]}; }`;
+            styleTag.appendChild(document.createTextNode(`.bet-grid-${betNumbers[index]} { width: 55px; height: 55px; border-radius: 50%; 
+                background-color: #ADC0C4; display: flex; justify-content: center; align-items: center; 
+                font-size: 20px; font-weight: 500; cursor: pointer; transition: .2s linear; }
+                .bet-grid-${betNumbers[index]}:hover { background-color: #919a9b; }`));
             const newElement = document.createElement('div');
-            newElement.className = betNumbers[i];
+            newElement.className = `bet-grid-${betNumbers[index]}`;
+            newElement.appendChild(document.createTextNode(betNumbers[index]));
             betNumbersElements.push(newElement);
+
+        }
+
+        for (let element in betNumbersElements) {
+
+            betGrid.appendChild(betNumbersElements[element])
 
         }
 
@@ -30,5 +41,41 @@ window.onload = () => {
     }
 
     populateBetGrid ();
+
+    function betNumbersEvents () {
+
+        for (let index = 0; index <= betNumbersElements.length; index++) {
+
+            betNumbersElements[index].addEventListener('click', () => {
+
+                if (markedNumbers.find(number => number == betNumbersElements[index].innerText)) {
+
+                    markedNumbers = markedNumbers.filter(number => number != betNumbersElements[index].innerText);
+                    betNumbersElements[index].className = `bet-grid-${index+1}`;
+
+                }
+
+                else {
+
+                    markedNumbers.push(betNumbersElements[index].innerText);
+                    betNumbersElements[index].className = 'selected';
+
+                }
+
+            });
+
+        }
+
+        return;
+
+    }
+
+    function handleEventListeners () {
+
+        betNumbersEvents();
+
+    }
+
+    handleEventListeners ();
 
 }
