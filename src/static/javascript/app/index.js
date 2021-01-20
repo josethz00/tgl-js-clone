421cc0de93f2b37fc119d14c8069f33d9633567b
josethz00 <jtsoares17@hotmail.com>
2021-01-20 11:07:59 -0300

    fixing: modified index.js to fill correctly the numbers

--- src/static/javascript/app/index.js
+++ src/static/javascript/app/index.js
@@ -14,7 +14,7 @@ window.onload = function () {
     const betSelections = document.querySelector('.bet-selections');
     const emptyCartAdvice = document.createElement('span');
 
-    let betNumbers = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
+    let betNumbers = [1, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
     let betNumbersElements = [];
     let markedNumbers = [];
     let ajaxResponse;
